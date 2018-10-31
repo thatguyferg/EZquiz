1c507bf916469b092687a09bf339dfb63a805800
Ferg <austen.a.ferguson@gmail.com>
2018-10-31 19:45:50 -0400

    Starting activity has a button that leads to QuizActivity

--- app/src/main/java/com/example/ferggot/ezquiz/WelcomeScreen.java
+++ app/src/main/java/com/example/ferggot/ezquiz/WelcomeScreen.java
@@ -1,7 +1,11 @@
 package com.example.ferggot.ezquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
 public class WelcomeScreen extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class WelcomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+
+
+        Button btn = (Button)findViewById(R.id.start_button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(WelcomeScreen.this, QuizActivity.class));
+            }
+        });
+
+
     }
+
+
+
 }
