2e785dcb7f496b878e8c723a1b50f01b1a48b1e1
Ferg <austen.a.ferguson@gmail.com>
2019-01-17 13:41:00 -0500

    Added a few files to .gitignore that caused issues when pulling a new build from git, TODO set up

--- app/src/androidTest/java/com/thatguyferg/ezquiz/WelcomeActivityAndroidUnitTest.java
+++ app/src/androidTest/java/com/thatguyferg/ezquiz/WelcomeActivityAndroidUnitTest.java
@@ -1,17 +1,40 @@
 package com.thatguyferg.ezquiz;
 
+import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.assertion.ViewAssertions.*;
+import androidx.test.espresso.matcher.ViewMatchers.*;
+import androidx.test.espresso.Espresso.*;
 
+import static androidx.test.espresso.Espresso.onView;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
 public class WelcomeActivityAndroidUnitTest {
+
 
     @Rule
     public ActivityTestRule<WelcomeActivity> welcomeActivityActivityTestRule
             = new ActivityTestRule<>(WelcomeActivity.class);
 
-
+   /* @Test
+    public void doesTextShowUp(){
+        onView(android.R.id.)
+    }*/
 }
+
+/*TODO
+1. More tests (use static imports from Espresso for now)
+1b. Get .gitignore set up properly to avoid silly incompatibilities
+2. Look into fixing import situation
+3. Fix app to work with rotations/other variables
+4. Add new tests if applicable
+ */
