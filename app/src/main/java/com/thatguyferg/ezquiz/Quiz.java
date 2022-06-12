b75de60dfd8f72455aee7ed69acf79d1e4cf498c
Austen Ferguson <austen.a.ferguson@gmail.com>
2022-06-12 18:36:16 -0400

    Fine tuning of landscape layout

--- app/src/main/java/com/thatguyferg/ezquiz/Quiz.java
+++ app/src/main/java/com/thatguyferg/ezquiz/Quiz.java
@@ -8,7 +8,9 @@ class Quiz extends AppCompatActivity{
 
     final List<Question> questionList = Arrays.asList (
             new Question(1, "Is milly a good dog?", "Yes", "Yeah", "Ye", Question.Answer.C),
-            new Question(2, "Does the next button work?", "Yes", "Maybe", "Definitely", Question.Answer.A)
+            new Question(2, "Does the next button work?", "Yes", "Maybe", "Definitely", Question.Answer.A),
+            new Question(3, "Is this going to break the layout?", "Yeah yeah yeah yeah yeah yeah yeah yeah yeah yeah yeah yeah", "Probably", "No", Question.Answer.A),
+            new Question(4, "Will this really really really really really really really really really really really long question do something bad?", "No", "Yes", "Both", Question.Answer.C)
     );
 
     int qIndex = 0;
